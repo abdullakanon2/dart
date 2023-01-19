64fe5e3856446cd7c40fdf7163b283730a84197c
abdullakanon2 <abdullakanon2@gmail.com>
2023-01-19 21:17:27 +0600

    joy bangla

---
+++ Dart/bin/main.dart
@@ -0,0 +1,16 @@
+void main() {
+  var name = "Abdulla";
+  int age = 30;
+
+  print(age);
+  print(name);
+
+  int a = 19;
+  int b = 20;
+  int c = 30;
+  var sum = a + b + c;
+  print(sum);
+  print(a + b - c);
+  print((a + b) * c);
+  print((a + b) / c);
+}
